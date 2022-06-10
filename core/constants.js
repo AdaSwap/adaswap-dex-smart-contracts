81ff550f49cac949d3a8e8acdf807e868fe02e32
Mihail Ceban <mihail.ceban@titanium-soft.com>
2022-06-10 17:24:19 +0300

    added constants and hided build

---
+++ core/constants.js
@@ -0,0 +1,11 @@
+module.exports = {
+    milkomedaTestnet: {
+        FEE_ADDRESS: '0x6C12887a13A4f88D8da142f588C1285FfdABC7e5'
+    },
+    polygonTestnet: {
+        FEE_ADDRESS: '0x6C12887a13A4f88D8da142f588C1285FfdABC7e5'
+    },
+    milkomedaMainnet: {
+        FEE_ADDRESS: ''
+    }
+};
