34ac7594db1edc6e058b8ee9f9114c43e79edfdb
Mihail Ceban <mihail.ceban@titanium-soft.com>
2022-06-13 14:38:06 +0300

    added constants.js and changed migration script for periphery

--- periphery/migrations/1_deploy_contracts.js
+++ periphery/migrations/1_deploy_contracts.js
@@ -1,18 +1,8 @@
-const Router = artifacts.require('AdaswapRouter02.sol')
+const Router = artifacts.require('AdaswapRouter02.sol');
+const constants = require('../constants');
 
 module.exports = async function (deployer, network) {
-  var weth;
-  var FACTORY_CONTRACT_ADDRESS;
-  switch (network) {
-    case "milkomedaTestnet":
-      FACTORY_CONTRACT_ADDRESS = '0xf2Fe389a25eD1c98cEf93B624c59e299433057Ee';
-      weth = {address: "0x65a51E52eCD17B641f8F0D1d56a6c9738951FDC9"};
-      break;
-    case "mainnet":
-      // initial pair contract code hash:
-      FACTORY_CONTRACT_ADDRESS = '';
-      weth = {address: ""};
-      break;
-  }
-  await deployer.deploy(Router, FACTORY_CONTRACT_ADDRESS, weth.address);
+  const { FACTORY_ADDRESS, WETH_ADDRESS } = constants[network];
+
+  await deployer.deploy(Router, FACTORY_ADDRESS, WETH_ADDRESS);
 };
