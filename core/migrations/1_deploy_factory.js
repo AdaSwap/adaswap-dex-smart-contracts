f333c3b49b6e064b06c3843e82bbf47b7a8e5e61
Dung Van <dung.van@prophetstor.com>
2022-06-28 12:39:58 +0700

    update testnet DEX periphery

--- core/migrations/1_deploy_factory.js
+++ core/migrations/1_deploy_factory.js
@@ -4,5 +4,5 @@ const constants = require('../constants');
 module.exports = async function (deployer, network, addresses) {
   const { FEE_ADDRESS } = constants[network];
   await deployer.deploy(Factory, FEE_ADDRESS);
-  await Factory.deployed()
+  await Factory.deployed();
 };
