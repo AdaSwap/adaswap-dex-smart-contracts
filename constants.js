5c602368bad088d3116c53f5099b4fe266a50f77
Mihail Ceban <mihail.ceban@titanium-soft.com>
2022-07-06 17:22:08 +0300

    configured periphery project

---
+++ constants.js
@@ -0,0 +1,14 @@
+module.exports = {
+    development: {
+        FACTORY_ADDRESS: '0x75c02E2Bb0a17b6Ec9673941a1e58E2A2aC0c057',
+        WETH_ADDRESS: '0x65a51E52eCD17B641f8F0D1d56a6c9738951FDC9'
+    },
+    milkomedaTestnet: {
+        FACTORY_ADDRESS: '0xAa293D2262cFE5bBCE618f26E582e8E338237c8c',
+        WETH_ADDRESS: '0x65a51E52eCD17B641f8F0D1d56a6c9738951FDC9'
+    },
+    milkomedaMainnet: {
+        FACTORY_ADDRESS: '',
+        WETH_ADDRESS: ''
+    }
+};
--- periphery/hardhat.config.js
+++ periphery/hardhat.config.js
@@ -1,4 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-waffle");
+const fs = require("fs");
+require('dotenv').config({path:'../.env'});
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -10,6 +13,26 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
+function mnemonic() {
+  try {
+    return fs.readFileSync("./mnemonic.txt").toString().trim();
+  } catch (e) {
+    if (defaultNetwork !== "localhost") {
+      console.log(
+        "☢️ WARNING: No mnemonic file created for a deploy account. Try `yarn run generate` and then `yarn run account`."
+      );
+    }
+  }
+  return "";
+}
+
+
+//
+// Select the network you want to deploy to here:
+//
+const defaultNetwork = "localhost";
+
+
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
@@ -17,5 +40,62 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
-};
+  defaultNetwork,
+
+  networks: {
+    hardhat: {
+      // forking: {
+      //   url: process.env.KOVAN_PROVIDER,
+      //   blockNumber: 30282748
+      // }
+    },
+    localhost: {
+      url: "http://localhost:8545",
+      /*      
+        notice no mnemonic here? it will just use account 0 of the hardhat node to deploy
+        (you can put in a mnemonic here to set the deployer locally)
+      
+      */
+    },
+
+    milkomedaTestnet: {
+      url: `${process.env.MILKOMEDA_TESTNET_PROVIDER}`,
+      accounts: [`${process.env.MILKOMEDA_TESTNET_DEPLOYER_PRIV_KEY}`],
+    },
+
+    milkomedaMainnet: {
+      url: `${process.env.MILKOMEDA_MAINNET_PROVIDER}`,
+      accounts: [`${process.env.MILKOMEDA_MAINNET_DEPLOYER_PRIV_KEY}`],
+    },
+
+    polygonTestnet: {
+      url: `${process.env.POLYGON_TESTNET_PROVIDER}`,
+      accounts: [`${process.env.POLYGON_TESTNET_DEPLOYER_PRIV_KEY}`],
+    },
+
+    // rinkeby: {
+    //   url: `https://rinkeby.infura.io/v3/${process.env.RINKEBY_INFURA_KEY}`,
+    //   accounts: [`${process.env.RINKEBY_DEPLOYER_PRIV_KEY}`],
+    // },
+    // mainnet: {
+    //   url: `https://mainnet.infura.io/v3/${process.env.MAINNET_INFURA_KEY}`,
+    //   accounts: [`${process.env.MAINNET_DEPLOYER_PRIV_KEY}`],
+    // },
+    // ropsten: {
+    //   url: `https://ropsten.infura.io/v3/${process.env.ROPSTEN_INFURA_KEY}`,
+    //   accounts: [`${process.env.ROPSTEN_DEPLOYER_PRIV_KEY}`],
+    // },
+
+  },
+
+  solidity: {
+    version: "0.8.13",
+    settings: {          // See the solidity docs for advice about optimization and evmVersion
+      optimizer: {
+        enabled: false,
+        runs: 999999
+      },
+      evmVersion: "istanbul",
+    }
+  }
+}
