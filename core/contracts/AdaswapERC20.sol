036d2834ffbbe88ed58e5f14f870f7dcbb81fc49
dungvan <dungvan2512@gmail.com>
2022-09-01 12:12:07 +0700

    not need to get chainid by assembly

--- core/contracts/AdaswapERC20.sol
+++ core/contracts/AdaswapERC20.sol
@@ -19,16 +19,12 @@ contract AdaswapERC20 is IAdaswapERC20 {
     mapping(address => uint) public override nonces;
 
     constructor() {
-        uint chainId;
-        assembly {
-            chainId := chainid()
-        }
         DOMAIN_SEPARATOR = keccak256(
             abi.encode(
                 keccak256('EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)'),
                 keccak256(bytes(name)),
                 keccak256(bytes('1')),
-                chainId,
+                block.chainid,
                 address(this)
             )
         );
