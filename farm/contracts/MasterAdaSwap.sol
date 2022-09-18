8c57260f4a111038a7f18682d3c623e521f435e0
nikegor7 <jojololstand@gmail.com>
2022-09-18 12:25:02 +0300

    Last Changes

--- farm/contracts/MasterAdaSwap.sol
+++ farm/contracts/MasterAdaSwap.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
-//import "hardhat/console.sol";
+
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "../node_modules/hardhat/console.sol";   //
+import "hardhat/console.sol";
 import "./libraries/Batchable.sol";
 import "./libraries/Number.sol";
 import "./interfaces/IRewarder.sol";
