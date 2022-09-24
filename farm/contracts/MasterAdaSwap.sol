b91d60a808b035791685e34cdb0c29617d46e785
Mihail Ceban <mihail.ceban@titanium-soft.com>
2022-09-24 14:27:25 +0300

    misha-clean-repo

--- farm/contracts/MasterAdaSwap.sol
+++ farm/contracts/MasterAdaSwap.sol
@@ -4,14 +4,12 @@ pragma solidity >=0.8.0 <0.9.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "hardhat/console.sol";
-import "./libraries/Batchable.sol";
 import "./libraries/Number.sol";
 import "./interfaces/IRewarder.sol";
 import "./interfaces/IMasterAdaSwap.sol";
 
 /// @notice The MasterAdaSwap (MO) contract gives out a constant number of ASW tokens per second by minting right from the AdaSwapToken contract.
-contract MasterAdaSwap is Ownable, Batchable {
+contract MasterAdaSwap is Ownable {
     using SafeERC20 for IERC20;
     using UInt256 for uint256;
     using Int256 for int256;
