b99a2f70746ec2fca61115942abde21c1b7439de
yerkebulanbltzr <yerkebulanbltzr@bltzr.gg>
2022-09-25 16:58:19 +0600

    fix: remove imports which already imported from another import

--- farm/contracts/MasterAdaSwap.sol
+++ farm/contracts/MasterAdaSwap.sol
@@ -2,9 +2,7 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "hardhat/console.sol";
 import "./libraries/Batchable.sol";
 import "./libraries/Number.sol";
 import "./interfaces/IRewarder.sol";
